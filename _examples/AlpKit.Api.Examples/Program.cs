4c51f42f82e271eb7772d5a5f53f3bfb5fe40572
alperbasd <125950998+alperbasd@users.noreply.github.com>
2025-04-28 22:53:59 +0300

    tests ok

--- _examples/AlpKit.Api.Examples/Program.cs
+++ _examples/AlpKit.Api.Examples/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddRedisCache(new CacheSettings { Host= "your_redis_host", Password= "your_redis_password", Port= "your_redis_port" });
+builder.Services.AddMemCache();
 
 builder.Services.AddOpenApi();
 
