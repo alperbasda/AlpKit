650e7bc7f48fb61e2e9c78ab8a2d704f1e97897c
alperbasd <125950998+alperbasd@users.noreply.github.com>
2025-04-30 12:36:47 +0300

    ok

---
+++ HelperKit/AlpKit.Helpers/CoderHelpers/Coder.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace AlpKit.Helpers.CoderHelpers;
+
+public class Coder
+{
+    public static string? DecodeFromBase64(string? base64EncodedData)
+    {
+        if (string.IsNullOrEmpty(base64EncodedData))
+            return null;
+
+        byte[] byteArray = Convert.FromBase64String(base64EncodedData);
+
+        return Encoding.UTF8.GetString(byteArray);
+    }
+
+    public static string? EncodeToBase64(string? plainText)
+    {
+        if (string.IsNullOrEmpty(plainText))
+            return null;
+
+        byte[] byteArray = Encoding.UTF8.GetBytes(plainText);
+
+        return Convert.ToBase64String(byteArray);
+    }
+}
+
